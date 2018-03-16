2e34670d574e93d08df2fea1827c1823937d891e
Grzegorz Otworowski <grzegorz.otworowski@student.put.poznan.pl>
2018-03-17 00:00:29 +0100

    XD

--- Swapper.cpp
+++ Swapper.cpp
@@ -1,120 +1,125 @@
-//made by Grzege
 #include<iostream>
 #include<fstream>
 #include<string>
 using namespace std;
 string ruch;
-void kafelki(int a[])
+int z=-1,n=-1;
+void kafelki(int a[4][4])
 {
-	for(int i=1;i<=16;i++)
+	for(int i=0;i<4;i++)
 	{
-		if(a[i]>0)cout<<" ";
-		cout<<a[i]<<" ";
-		if(i%4==0)cout<<endl;
+		for(int j=0;j<4;j++)
+			{
+				if(a[i][j]>0)cout<<" ";
+				cout<<a[i][j]<<" ";
+			}
+			cout<<endl;	
 	}
-	cout<<endl;
 }
-void wypisz_wymaluj(int x, int y)
+void wypisz_wymaluj(int a[4][4],int x, int y)
 {
-	for(int i=1;i<=16;i++)
+	for(int i=0;i<16;i++)
 	{
 		if(i==x||i==y)ruch+="x ";
 		else ruch+="o ";
-		if(i%4==0)ruch+="\n";
+		if((i+1)%4==0)ruch+="\n";
 	}
 }
-int szukajta (int a[],int x, int pw1, int pw2)
+int szukajta (int a[4][4],int x, int r1, int k1, int r2, int k2)
 {
-	for(int i=1;i<=16;i++)
+	//cout<<r1<<" "<<k1<<" "<<r2<<" "<<k2<<"\n";
+	for(int i=0;i<4;i++)
 	{
-		if(a[i]==x&&i!=pw1&&i!=pw2) 
+		for(int j=0;j<4;j++)
 		{
-			return i;
-			break;
+			if((a[i][j]==x)&&(i!=r1||j!=k1)&&(i!=r2||j!=k2))return 4*i+j;
 		}
 	}
 	return -1;
 }
+void horizontal(int a[4][4])
+{
+	for(int i=0;i<4;i++)
+	{
+		for(int j=0;j<2;j++)
+		{
+			if (a[i][j]==a[i][j+2]&&a[i][j]>0&&a[i][j+1]>0&&a[i][j+2]>0)
+			{
+				//cout<<"y1"<<endl;
+				z=szukajta(a,a[i][j],i,j,i,j+2);
+				n=i*4+j+1;
+				if(z>=0)break;
+			}
+			else if (a[i][j]==a[i][j+1]&&a[i][j]>0&&a[i][j+1]>0&&a[i][j+2]>0)
+			{
+				//cout<<"y2"<<endl;
+				z=szukajta(a,a[i][j],i,j,i,j+1);
+				n=i*4+j+2;
+				if(z>=0)break;
+			}
+			else if(a[i][3-j]==a[i][2-j]&&a[i][3-j]>0&&a[i][2-j]>0&&a[i][1-j]>0)
+			{
+				//cout<<a[i][3-j]<<" "<<a[i][2-j]<<" y3"<<endl;
+				z=szukajta(a,a[i][3-j],i,3-j,i,2-j);
+				n=i*4+1-j;
+				if(z>=0)break;
+			}
+		}
+		if(z>=0)break;
+	}
+}
+void vertical(int a[4][4])
+{
+	for(int j=0;j<4;j++)
+	{
+		for(int i=0;i<2;i++)
+		{
+			if (a[i][j]==a[i+2][j]&&a[i][j]>0&&a[i+1][j]>0&&a[i+2][j]>0)
+			{
+				cout<<i<<" "<<j<<" "<<"y1"<<endl;
+				z=szukajta(a,a[i][j],i,j,i+2,j);
+				n=(i+1)*4+j;
+				if(z>=0)break;
+			}
+			else if (a[i][j]==a[i+1][j]&&a[i][j]>0&&a[i+1][j]>0&&a[i+2][j]>0)
+			{
+				cout<<"y2"<<endl;
+				z=szukajta(a,a[i][j],i,j,i+1,j);
+				n=(i+2)*4+j;
+				if(z>=0)break;
+			}
+			else if(a[3-i][j]==a[2-i][j]&&a[3-i][j]>0&&a[2-i][j]>0&&a[1-i][j]>0)
+			{
+				cout<<a[i][3-j]<<" "<<a[i][2-j]<<" y3"<<endl;
+				z=szukajta(a,a[3-i][j],3-i,j,2-i,j);
+				n=(1-i)*4+j;
+				if(z>=0)break;
+			}
+		}
+		if(z>=0)break;
+	}
+}
 int main(){
 	fstream plik;
 	plik.open("plansza.txt",ios::in);
-	int a[17];
-		for(int i=1;i<=16;i++)
+	int a[4][4];
+	for(int i=0;i<4;i++)
 	{
-		plik>>a[i];
+		for(int j=0;j<4;j++)
+		{
+			plik>>a[i][j];
+		}
 	}
 	kafelki(a);
 	plik.close();
 	plik.open("ruch.txt",ios::out);
 	//plik<<"x o o o\no o o o\no x o o\no o o o";
-	for(int k=0;k<=1;k++)//k=0 l->p v g->d, k=1 p->l v d->g 
-	{
-		for(int j=1;j<=2;j++) //j=1 (1-2-3) j=2 (2-3-4)
-		{
-			for(int i=0;i<16;i+=4)//i->rz¹d planszy numerowany od 0, ró¿nica pionowa pomiêdzy rzêdami wynosi 4
-			{
-				int h[3],v[3];
-				h[0]=(2-j)*k*3+(j-1)*k+j+i;//j=1,k=1|j=2,k=1|j=1vj=2 k=0
-				h[1]=(2-j)*k+(j-1)*(-k)+(j+1)+i;
-				h[2]=(2-j)*(-k)+(j-1)*(-k)*3+(j+2)+i;
-				v[0]=(2-j)*k*12+(j-1)*k*4+(j-1)*3+j+i/4;//j=1,k=1|j=2,k=1|j=1vj=2 k=0
-				v[1]=(2-j)*k*4+(j-1)*k*(-4)+(j-1)*3+j+4+i/4;
-				v[2]=(2-j)*k*(-4)+(j-1)*k*(-12)+(j-1)*3+j+8+i/4;
-				if((a[h[0]]==a[h[1]])&&a[h[2]]>0&&a[h[0]]>0&&a[h[1]]>0)
-				{
-					//cout<<h[0]<<" "<<h[1]<<endl;
-					int z=szukajta(a,a[h[0]],h[0],h[1]);
-					if(z>0)
-					{
-						wypisz_wymaluj(h[2],z);
-						plik<<ruch;
-						cout<<ruch;
-						plik.close();
-						return 0;
-					}
-				}
-				if((a[h[0]]==a[h[2]])&&a[h[2]]>0&&a[h[0]]>0&&a[h[1]]>0)
-				{
-					//cout<<h[0]<<" "<<h[1]<<endl;
-					int z=szukajta(a,a[h[0]],h[0],h[2]);
-					if(z>0)
-					{
-						wypisz_wymaluj(h[1],z);
-						plik<<ruch;
-						cout<<ruch;
-						plik.close();
-						return 0;
-					}
-				}
-				if((a[v[0]]==a[v[1]])&&a[v[2]]>0&&a[v[0]]>0&&a[v[1]]>0)
-				{
-					//cout<<h[0]<<" "<<h[1]<<endl;
-					int z=szukajta(a,a[v[0]],v[0],v[1]);
-					if(z>0)
-					{
-						wypisz_wymaluj(v[2],z);
-						plik<<ruch;
-						cout<<ruch;
-						plik.close();
-						return 0;
-					}
-				}
-				if((a[v[0]]==a[v[2]])&&a[v[2]]>0&&a[v[0]]>0&&a[v[1]]>0)
-				{
-					//cout<<v[0]<<" "<<v[2]<<endl;
-					int z=szukajta(a,a[v[0]],v[0],v[2]);
-						//cout<<z<<" "<<v[1]<<endl;
-					if(z>0)
-					{
-						wypisz_wymaluj(v[1],z);
-						plik<<ruch;
-						cout<<ruch;
-						plik.close();
-						return 0;
-					}
-				}
-			}
-		}
-	}
-
+	horizontal(a);
+	if(z<0)vertical(a);
+	cout<<"z="<<z<<"\t n="<<n<<endl;
+	if(z>=0)wypisz_wymaluj(a,z,n);
+	plik<<ruch;
+	cout<<ruch;
+	plik.close();
+	return 0;
 }
