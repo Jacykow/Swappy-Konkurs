9bda1e14d0eda257464795b8aa7d5aa661e31358
Grzegorz Otworowski <grzegorz.otworowski@student.put.poznan.pl>
2018-03-17 13:47:57 +0100

    Update
    !Komentarze

--- Swapper.cpp
+++ Swapper.cpp
@@ -27,7 +27,6 @@ void wypisz_wymaluj(int a[4][4],int x, int y)
 }
 int szukajta (int a[4][4],int x, int r1, int k1, int r2, int k2)
 {
-	//cout<<r1<<" "<<k1<<" "<<r2<<" "<<k2<<"\n";
 	for(int i=0;i<4;i++)
 	{
 		for(int j=0;j<4;j++)
@@ -49,11 +48,15 @@ void kareta(int a[4][4],int w, int x, int y, int hv)
 			else if(hv==0&&((a[i][j]==w)&&(j!=y)))z=4*i+j;
 		}
 	}
-	if(z>=0)wypisz_wymaluj(a,z,n);
-	k=0;
+	if(z>=0)
+	{
+		wypisz_wymaluj(a,z,n);
+		k=0;
+	}
+
 }
 void horizontal(int a[4][4])
-	{
+{
 	int h[4][4]={0};
 	for(int i=0;i<4;i++)
 	{
@@ -70,18 +73,12 @@ void horizontal(int a[4][4])
 				if((a[i][j]==a[i][r])&&j!=r)
 				{
 				h[i][j]++;
-				//cout<<a[i][j]<<"<?>"<<a[i][r]<<endl;
 				}
 				else if(j!=r)p=r;
 			}
-			//cout<<h[i]<<" ";
-			/*if((j<2&&j>0)&&((a[i][j]==a[i][j+1]&&a[i][j+1]==a[i][j-1])||(a[i][j]==a[i][2+j]&&a[i][j]==a[i][0])))h[i]++;
-			else if(a[i][j]==a[i][j+1])h[i]++;
-			else if(a[i][j]!=a[i][j-1]||a[i][j]!=a[i][j-2])p=j;*/
 			if(p==-1)p=3;
 			if(h[i][j]==2)
 			{
-				cout<<"hv "<<p<<"\n";
 				if(p>0)w=a[i][p-1];
 				else w=a[i][p+1];
 				kareta(a,w,p,i,1);	
@@ -90,28 +87,24 @@ void horizontal(int a[4][4])
 		}
 	if(!k)break;
 	}
-		//cout<<endl;
 	if(z<0&&k)for(int i=0;i<4;i++)
 	{
 		for(int j=0;j<2;j++)
 		{
-			if (a[i][j]==a[i][j+2]&&a[i][j]>0&&a[i][j+1]>0&&a[i][j+2]>0)//0x2 v 1x3
+			if (a[i][j]==a[i][j+2]&&a[i][j]>0&&a[i][j+1]>0&&a[i][j+2]>0)
 			{
-				//cout<<"y1"<<endl;
 				z=szukajta(a,a[i][j],i,j,i,j+2);
 				n=i*4+j+1;
 				if(z>=0)break;
 			}
-			else if (a[i][j]==a[i][j+1]&&a[i][j]>0&&a[i][j+1]>0&&a[i][j+2]>0)//01x v 12x
+			else if (a[i][j]==a[i][j+1]&&a[i][j]>0&&a[i][j+1]>0&&a[i][j+2]>0)
 			{
-				//cout<<"y2"<<endl;
 				z=szukajta(a,a[i][j],i,j,i,j+1);
 				n=i*4+j+2;
 				if(z>=0)break;
 			}
-			else if(a[i][3-j]==a[i][2-j]&&a[i][3-j]>0&&a[i][2-j]>0&&a[i][1-j]>0)//32x v 21x
+			else if(a[i][3-j]==a[i][2-j]&&a[i][3-j]>0&&a[i][2-j]>0&&a[i][1-j]>0)
 			{
-				//cout<<a[i][3-j]<<" "<<a[i][2-j]<<" y3"<<endl;
 				z=szukajta(a,a[i][3-j],i,3-j,i,2-j);
 				n=i*4+1-j;
 				if(z>=0)break;
@@ -138,25 +131,19 @@ void vertical(int a[4][4])
 				if((a[i][j]==a[r][j])&&i!=r)
 				{
 				v[i][j]++;
-				cout<<v[i][j]<<" "<<a[i][j]<<"<?>"<<a[r][j]<<endl;
 				}
 				else if(i!=r)p=r;
 			}
-			//cout<<h[i]<<" ";
-			/*if((j<2&&j>0)&&((a[i][j]==a[i][j+1]&&a[i][j+1]==a[i][j-1])||(a[i][j]==a[i][2+j]&&a[i][j]==a[i][0])))h[i]++;
-			else if(a[i][j]==a[i][j+1])h[i]++;
-			else if(a[i][j]!=a[i][j-1]||a[i][j]!=a[i][j-2])p=j;*/
 			if(p==-1)p=3;
 			if(v[i][j]==2)
 			{
-				cout<<"kv "<<p<<"\n";
 				if(p>0)w=a[p-1][j];
 				else w=a[p+1][j];
 				kareta(a,w,p,j,0);	
 				break;
 			}
 		}
-	if(!k)break;
+		if(!k)break;
 	}
 	if(z<0&&k)for(int j=0;j<4;j++)
 	{
@@ -164,21 +151,18 @@ void vertical(int a[4][4])
 		{
 			if (a[i][j]==a[i+2][j]&&a[i][j]>0&&a[i+1][j]>0&&a[i+2][j]>0)
 			{
-				//cout<<i<<" "<<j<<" "<<"y1"<<endl;
 				z=szukajta(a,a[i][j],i,j,i+2,j);
 				n=(i+1)*4+j;
 				if(z>=0)break;
 			}
 			else if (a[i][j]==a[i+1][j]&&a[i][j]>0&&a[i+1][j]>0&&a[i+2][j]>0)
 			{
-				//cout<<"y2"<<endl;
 				z=szukajta(a,a[i][j],i,j,i+1,j);
 				n=(i+2)*4+j;
 				if(z>=0)break;
 			}
 			else if(a[3-i][j]==a[2-i][j]&&a[3-i][j]>0&&a[2-i][j]>0&&a[1-i][j]>0)
 			{
-				//cout<<a[i][3-j]<<" "<<a[i][2-j]<<" y3"<<endl;
 				z=szukajta(a,a[3-i][j],3-i,j,2-i,j);
 				n=(1-i)*4+j;
 				if(z>=0)break;
@@ -201,10 +185,8 @@ int main(){
 	kafelki(a);
 	plik.close();
 	plik.open("ruch.txt",ios::out);
-	//plik<<"x o o o\no o o o\no x o o\no o o o";
 	horizontal(a);
 	if(k)vertical(a);
-	cout<<"z="<<z<<"\t n="<<n<<"\t k="<<k<<endl;
 	if(z>=0&&k)wypisz_wymaluj(a,z,n);
 	plik<<ruch;
 	cout<<ruch;
